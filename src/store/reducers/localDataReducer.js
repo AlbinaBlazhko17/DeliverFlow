2610892bd3122a820e8530ff09d0178a89536534
Albinator <albinamivina@gmail.com>
2023-08-24 21:52:05 +0300

    remove unnecessary data

--- src/store/reducers/localDataReducer.js
+++ src/store/reducers/localDataReducer.js
@@ -9,7 +9,6 @@ import {
 
 const initialState = {
   results: [],
-  sortType: '',
 };
 export const sortRequestsByDate = (sortByDateDispatch) => ({
   type: 'localData/sortRequestsByDate',
@@ -20,12 +19,6 @@ const localData = createReducer(initialState, (builder) => {
   builder
     .addCase(loadFromLocalStorage, (state, action) => {
       const storedData = [];
-
-      // for (let i = 0; i < localStorage.length; i++) {
-      //   const key = localStorage.key(i);
-      //   const value = JSON.parse(localStorage.getItem(key));
-      //   storedData[key] = value;
-      // }
 
       for (let i = 0; i < localStorage.length; i++) {
         const key = localStorage.key(i);
@@ -38,21 +31,8 @@ const localData = createReducer(initialState, (builder) => {
       }
 
       return state;
-      // if (storedData) {
-      //   return { ...state, results: storedData };
-      // }
-
-      // return state;
     })
     .addCase(addRequest, (state, action) => {
-      // const newItem = { ...action.payload };
-      // state.results[newItem.id] = newItem;
-      // const updatedResults = { ...state.results };
-
-      // localStorage.setItem(newItem.id, JSON.stringify(newItem));
-
-      // return { ...state, results: updatedResults };
-
       const newItem = { ...action.payload };
       const updatedResults = [...state.results, newItem];
 
@@ -60,27 +40,12 @@ const localData = createReducer(initialState, (builder) => {
       return { ...state, results: updatedResults };
     })
     .addCase(updateRequest, (state, action) => {
-      // const updatedItem = action.payload;
-
-      // const updatedResults = {
-      //   ...state.results,
-      //   [updatedItem.id]: updatedItem,
-      // };
-
-      // localStorage.setItem(updatedItem.id, JSON.stringify(updatedItem));
-
-      // return { ...state, results: updatedResults };
       const updatedItem = action.payload;
-      // Update the local storage
+
       localStorage.setItem(updatedItem.id, JSON.stringify(updatedItem));
       return state;
     })
     .addCase(removeRequest, (state, action) => {
-      // const idToRemove = action.payload.id;
-      // localStorage.removeItem(idToRemove);
-      // const updatedResults = { ...state.results };
-      // delete updatedResults[idToRemove];
-      // return { ...state, results: updatedResults };
       const idToRemove = action.payload.id;
       localStorage.removeItem(idToRemove);
       return {
@@ -89,25 +54,6 @@ const localData = createReducer(initialState, (builder) => {
       };
     })
     .addCase(sortRequests, (state, action) => {
-      // const sortByDispatch = action.payload.sortType;
-      // const sortedResults = { ...action.payload.requests };
-
-      // const sortedArray = Object.values(sortedResults).sort((a, b) => {
-      //   const dateA = sortByDispatch === 'dispatch' ? a.date : a.dateOfCreation;
-      //   const dateB = sortByDispatch === 'dispatch' ? b.date : b.dateOfCreation;
-
-      //   const parsedDateA = Date.parse(dateA.split('/').reverse().join('-'));
-      //   const parsedDateB = Date.parse(dateB.split('/').reverse().join('-'));
-
-      //   return parsedDateB - parsedDateA;
-      // });
-
-      // const updatedSortedResults = {};
-      // sortedArray.forEach((item) => {
-      //   updatedSortedResults[item.id] = item;
-      // });
-
-      // return { ...state, results: updatedSortedResults };
       const sortByDispatch = action.payload.sortType;
       const data = [...action.payload.requests];
       const itemsWithDate = [];
